ec1ec6bea15cc61be732ea4518a26941bd498431
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-03 11:26:50 -0700

    update program logic

--- WeekdayFinder/Program.cs
+++ WeekdayFinder/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using WeekdayFinder.Models;
 
 namespace WeekdayFinder
@@ -9,7 +10,33 @@ namespace WeekdayFinder
   {
     public static void Main()
     {
-      // code goes here
+      Console.WriteLine("Enter a year:");
+      int year = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter a month(number):");
+      int month = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter a day:");
+      int day = int.Parse(Console.ReadLine());
+      try
+      {
+        DateTime date = new DateTime(year, month, day);
+        Calendar newCalendar = CultureInfo.InvariantCulture.Calendar;
+        Console.WriteLine(newCalendar.GetDayOfWeek(date));
+      }
+      catch(Exception invalidDate)
+      {
+        Console.WriteLine(invalidDate.Message);
+        Console.WriteLine("Please enter a date between 01/01/0001 and 12/31/9999");
+      }
+      Console.WriteLine("Would you like to check another date? (yes/no)");
+      string response = Console.ReadLine();
+      if (response == "yes")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Goodbye");
+      }
     }
   }
 }
